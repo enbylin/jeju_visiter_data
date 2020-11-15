d81650fa838b30e798f828e44307e296be26a696
enbylin <enbylin@gmail.com>
2020-11-15 13:14:07 +0900

    스프레드시트 셀에서 지역명 받고 DataFrame 만들기
    예외처리 적용함.
    없는 지역명일 경우 특정셀에 문구 업데이트

--- sp_connect.py
+++ sp_connect.py
@@ -15,27 +15,33 @@ sheet_name = "api_test"  # 파일명 변수 저장
 client = gspread.authorize(creds)  # 연동 및 로그인 인증
 sps = client.open(sheet_name)  # 파일 연동 후 저장
 
-tab1_sheet = sps.worksheet("tab1")  # 파일내 워크시트 선택 후 저장
+tab_info_sheet = sps.worksheet("info")  # 파일내 워크시트 선택 후 저장
+tab1_sheet = sps.worksheet("tab1")  
 tab2_sheet = sps.worksheet("tab2")
 tab3_sheet = sps.worksheet("tab3")
 
 ### 업데이트 영역 ###
 
+# 특정 셀 값 가져오기
+poi = tab_info_sheet.cell(1, 1).value
+
 # DataFrame create
-poi = '천지연폭포'
-poi_code = ac.contents_connet(poi)
+try:
+    poi_code = ac.contents_connet(poi)
+    tab_info_sheet.update_acell("A4", " ")
 
-age_df = ac.visitor_age(poi_code)
-cnt_df = ac.visitor_cnt(poi_code)
-gender_df = ac.visitor_gender(poi_code)
+    age_df = ac.visitor_age(poi_code)
+    cnt_df = ac.visitor_cnt(poi_code)
+    gender_df = ac.visitor_gender(poi_code)    
 
-# 셀 업데이트
-# tab1_sheet.update_acell("A2", "AAA")
+    def sheet_update(sheet_obj, df):
+        sheet_obj.update([df.columns.values.tolist()] + df.values.tolist())
 
-def sheet_update(sheet_obj, df):
-    sheet_obj.update([df.columns.values.tolist()] + df.values.tolist())
+    sheet_update(tab1_sheet, age_df)
+    sheet_update(tab2_sheet, cnt_df)
+    sheet_update(tab3_sheet, gender_df)
 
+except:
+    # 셀 업데이트
+    tab_info_sheet.update_acell("A4", "없는 지역명입니다. 다시 입력해주세요")
 
-sheet_update(tab1_sheet, age_df)
-sheet_update(tab2_sheet, cnt_df)
-sheet_update(tab3_sheet, gender_df)
